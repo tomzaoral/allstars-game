9649a30bbcf6bf59d248d3fc715e832828ef1145
Tomas Zaoral <tzaoral@purple-technology.com>
2016-03-05 14:17:34 +0100

    Refactored monters positioning

--- allstars-game/ViewController.swift
+++ allstars-game/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var crosshair: UIImageView!
     @IBOutlet weak var score: UILabel!
     
@@ -24,61 +24,58 @@ class ViewController: UIViewController {
         
         addMonster()
         addMonster()
-		addMonster()
-		addMonster()
-		addMonster()
-		
+        addMonster()
+        addMonster()
+        addMonster()
+        
     }
     
     var monsters: [Monster] = []
-	
-	var lastPosition: CGPoint = CGPoint(x: 960, y: 540)
+    
+    var lastPosition: CGPoint = CGPoint(x: 960, y: 540)
     
     var scoreValue: Int = 0 {
         didSet {
-            score.text = "\(scoreValue)"
+//            score.text = "\(scoreValue)"
         }
     }
     
     func move(sender: UIPanGestureRecognizer) {
         
         if sender.state == .Ended {
-			
-			lastPosition = crosshair.center
-			
+            
+            lastPosition = crosshair.center
+            
         } else if sender.state == .Changed {
-			
+            
             let translation = sender.translationInView(self.view)
-			
-			let newX = max(min(lastPosition.x + translation.x, view.frame.width), 0)
-			let newY = max(min(lastPosition.y + translation.y, view.frame.height), 0)
-			
-			crosshair.center = CGPoint(x: newX, y: newY)
-
-		}
+            
+            let newX = max(min(lastPosition.x + translation.x, view.frame.width), 0)
+            let newY = max(min(lastPosition.y + translation.y, view.frame.height), 0)
+            
+            crosshair.center = CGPoint(x: newX, y: newY)
+            
+        }
         
     }
     
     func fire() {
         let crosshairPosition = crosshair.center
-		
-		let index = monsters.indexOf { CGRectContainsPoint($0.imageView!.frame, crosshairPosition) }
-		
-		if let i = index {
-			let monster = monsters[i]
+        
+        let index = monsters.indexOf { CGRectContainsPoint($0.imageView!.frame, crosshairPosition) }
+        
+        if let i = index {
+            let monster = monsters[i]
             
             scoreValue++
-			
-			let randomHeight = arc4random_uniform(UInt32(view.frame.height - 200)) + 1
-			let randomWidth = arc4random_uniform(UInt32(view.frame.width - 200)) + 1
-			
-			monster.imageView?.center = CGPoint(x: Int(randomWidth), y: Int(randomHeight))
-			
-			monster.imageView?.image = UIImage(named: "monster_\(arc4random_uniform(3) + 1)")
-			
-			print(monster.imageView?.center)
-		}
-
+            
+            monster.imageView?.frame = getCoords()
+            
+            monster.imageView?.image = UIImage(named: "monster_\(arc4random_uniform(3) + 1)")
+            
+            print(monster.imageView?.center)
+        }
+        
     }
     
     func addMonster() {
@@ -86,11 +83,8 @@ class ViewController: UIViewController {
         
         let imageview = UIImageView(image: UIImage(named: "monster_\(arc4random_uniform(3) + 1)"))
         
-        let randomHeight = arc4random_uniform(UInt32(view.frame.height - 200)) + 1
-        let randomWidth = arc4random_uniform(UInt32(view.frame.width - 200)) + 1
-
-        imageview.frame = CGRect(origin: CGPoint(x: Int(randomWidth), y: Int(randomHeight)), size: CGSize(width: 200, height: 200))
-		
+        imageview.frame = getCoords()
+        
         view.insertSubview(imageview, belowSubview: crosshair)
         
         let monster = Monster()
@@ -99,8 +93,14 @@ class ViewController: UIViewController {
         monsters.append(monster)
     }
     
-
-
+    func getCoords() ->CGRect {
+        let randomY = arc4random_uniform(UInt32(view.frame.height - 200)) + 1
+        let randomX = arc4random_uniform(UInt32(view.frame.width - 200)) + 1
+        
+        return CGRect(origin: CGPoint(x: Int(randomX), y: Int(randomY)), size: CGSize(width: 200, height: 200))
+    }
+    
+    
 }
 
 class Monster {
