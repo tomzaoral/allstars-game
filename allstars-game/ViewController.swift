324562ce29a6d427fbcee9ee889c4e5c7b7be3b8
machl <lukas.machalik@strv.com>
2016-03-05 11:40:02 +0100

    working move

--- allstars-game/ViewController.swift
+++ allstars-game/ViewController.swift
@@ -14,39 +14,29 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
         let pan = UIPanGestureRecognizer(target: self, action: "move:")
         self.view.addGestureRecognizer(pan)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 	
 	var lastPosition: CGPoint = CGPoint(x: 960, y: 540)
     
     func move(sender: UIPanGestureRecognizer) {
-		
-        if sender.state == .Began {
-            
-        }
         
         if sender.state == .Ended {
+			
 			lastPosition = crosshair.center
-        }
-		
-        if sender.state == .Changed {
+			
+        } else if sender.state == .Changed {
+			
             let translation = sender.translationInView(self.view)
 			
-			let newX = lastPosition.x + translation.x
-			let newY = lastPosition.y + translation.y
-        
-//            if 0...view.frame.height ~= newX && 0...view.frame.width ~= newY {
-                crosshair.center = CGPoint(x: newX, y: newY)
-//            }
-        }
+			let newX = max(min(lastPosition.x + translation.x, 1920), 0)
+			let newY = max(min(lastPosition.y + translation.y, 1080), 0)
+			
+			crosshair.center = CGPoint(x: newX, y: newY)
+
+		}
         
     }
 
