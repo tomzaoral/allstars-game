1849e9d72616fcd3136af002ac552161a6760f3e
machl <lukas.machalik@strv.com>
2016-03-05 17:17:00 +0100

    Add Game Over screen

---
+++ allstars-game/EndViewController.swift
@@ -0,0 +1,21 @@
+//
+//  EndViewController.swift
+//  allstars-game
+//
+//  Created by Lukas Machalik on 05/03/16.
+//  Copyright © 2016 STRV Hackaton. All rights reserved.
+//
+
+import UIKit
+
+class EndViewController: UIViewController {
+
+	@IBOutlet weak var timeLabel: UILabel!
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
